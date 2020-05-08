4eb91ef5c94e50d11bbbb924c8a933bb508fa21c
Aryan Varshney <54534428+thearyanvarshney@users.noreply.github.com>
2020-05-08 22:10:30 +0530

    Update ocloud.sql

--- ocloud.sql
+++ ocloud.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: May 04, 2020 at 06:31 PM
--- Server version: 5.7.26
--- PHP Version: 7.2.18
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2020 at 06:39 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,26 +24,82 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `signup`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `signup`;
-CREATE TABLE IF NOT EXISTS `signup` (
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `fname` varchar(50) NOT NULL,
   `lname` varchar(50) NOT NULL,
   `dob` date NOT NULL,
   `phone` bigint(15) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `cpassword` varchar(50) NOT NULL
+  `password` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `signup`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `signup` (`fname`, `lname`, `dob`, `phone`, `username`, `password`, `cpassword`) VALUES
-('Ritik', 'Nag', '1999-09-13', 7897094840, 'nagvanshi', '123456', 'cpass');
+INSERT INTO `user` (`id`, `fname`, `lname`, `dob`, `phone`, `username`, `password`) VALUES
+(1, 'SMS', 'Sendie', '2020-05-11', 488, 'abkg', 'password'),
+(2, 'Aryan', 'Varshney', '0000-00-00', 9457357157, 'iamaryan', 'abcdef'),
+(3, 'yogesh', 'sharma', '0000-00-00', 123456789, 'yogesh', '123456'),
+(4, 'shivam', 'sharma', '0000-00-00', 123456789, 'shivam', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userfiles`
+--
+
+CREATE TABLE `userfiles` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `caption` varchar(100) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userfiles`
+--
+
+INSERT INTO `userfiles` (`id`, `username`, `title`, `caption`, `filename`, `date`) VALUES
+(15, 'iamaryan', 'aaa', 'aaa', 'pexels-photo-1269968.jpeg', '2020-05-08');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userfiles`
+--
+ALTER TABLE `userfiles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `userfiles`
+--
+ALTER TABLE `userfiles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
