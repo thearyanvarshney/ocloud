59d541341864f3c0765284e2f98a374d8d7bec8b
Anuj Arora <anuj.arora28@gmail.com>
2020-05-16 14:24:11 +0530

    .

--- userinterface.php
+++ userinterface.php
@@ -41,7 +41,7 @@ charset=UTF-8">
 
 			<!--DIV ==> Logout Div-->
 
-				< a href="logout.php" button id="logout" class="button-four" onclick="window.open('login.php','_self')">Sign Out</button>
+				<a href="logout.php"><button id="logout" class="button-four" onclick="window.open('logout.php','_self')">Sign Out</button>
 
 		</div>
 
