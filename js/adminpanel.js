c0672fab9628d619fc53d4fc253d7c11b376a3d9
Anuj Arora <anuj.arora28@gmail.com>
2020-05-18 13:10:05 +0530

    .

---
+++ js/adminpanel.js
@@ -0,0 +1,9 @@
+function showdivuser() {
+  document.getElementById("issueblockview").style.display = "none";
+  document.getElementById('userdetail').style.display = "inline-block";
+}
+
+function showdivissue() {
+  document.getElementById('userdetail').style.display = "none";
+  document.getElementById("issueblockview").style.display = "inline-block";
+}
