9b05bc9591b23b21e9975ea38cc3f1e6dcd4cbde
Anuj Arora <anuj.arora28@gmail.com>
2020-05-18 14:48:39 +0530

    .

--- js/adminlogin.js
+++ js/adminlogin.js
@@ -1,15 +1,14 @@
-function validate()
-{
-  var id=document.getElementById('adminid').value;
-  var pass=document.getElementById('adminpass').value;
-  if(id!="admin" && pass!="admin2020")
-  {
-    alert("Incorrect Details!");
-    document.getElementById("signupform").action = "javascript:void(0)";
-    document.getElementById("signupform").method = "";
-  }
-  else {
-    document.getElementById("signupform").action = "adminpanel.html";
-    document.getElementById("signupform").method = "post";
+function validate() {
+  var id = document.getElementById('adminid').value;
+  var pass = document.getElementById('adminpass').value;
+  if (id.length != 0 && pass.length != 0) {
+    if (id != "admin" && pass != "admin2020") {
+      alert("Incorrect Details!");
+      document.getElementById("adminform").action = "javascript:void(0)";
+      document.getElementById("adminform").method = "";
+    } else {
+      document.getElementById("adminform").action = "adminpanel.php";
+      document.getElementById("adminform").method = "post";
+    }
   }
 }
