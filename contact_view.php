0672529e104e8930ea8ad6f2e4dbe5e2d76d026c
Aryan Varshney <54534428+thearyanvarshney@users.noreply.github.com>
2020-05-19 12:08:52 +0530

    Create contact_view.php

---
+++ contact_view.php
@@ -0,0 +1,20 @@
+<?php
+include 'dbconfig.php';
+if(isset($_REQUEST['submit'])){
+    $name = $_REQUEST['name'];
+    $phone=$_REQUEST['phone'];
+    $email = $_REQUEST['email'];
+    $comment = $_REQUEST['comment'];
+    $que="select * from contact where binary id='$user'";
+    $x=mysqli_query($conn,$que);
+if(mysqli_num_rows($x)==1)
+{
+    header('location:index.php?msg=Again Sent Your Query');
+}
+else{
+    $sql="INSERT INTO `contact` (`name`, `phone`, `email`, `comment`) VALUES ('$name','$phone','$email','$comment')";
+    mysqli_query($conn,$sql);
+    header('location:index.php?msg=Query Sent Succesfully');
+}
+}
+?>
