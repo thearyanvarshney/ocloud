c586e3bff612713fc70ab2e5bd643bfd1d0b708e
Anuj Arora <anuj.arora28@gmail.com>
2020-06-01 16:56:52 +0530

    .

---
+++ mailscript.php
@@ -0,0 +1,3 @@
+<?php
+mail('anuj.arora28@gmail.com','Test','HelloThere!','From: ocloudbca@gmail.com')
+?>
--- signup.php
+++ signup.php
@@ -24,18 +24,14 @@
     <div id="signupformdesign">
     <form id="signupform" action="login.php" method="post" autocomplete="off">
      <h1 style="font-family: myfont; font-size: 40px;">Let's Get Started!</h1>
-        <input class="inputclass" type="text" name="FirstName" placeholder="First Name" required>
-        <input class="inputclass" type="text" name="LastName" placeholder="Last Name">
-        <br><br>
+        <input class="inputclass inlineinput" type="text" name="FirstName" placeholder="First Name" required>
+        <input class="inputclass inlineinput" type="text" name="LastName" placeholder="Last Name">
         <input class="inputclass" type="date" name="DateOfBirth" placeholder="Date Of Birth" required>
-        <br><br>
+        <input class="inputclass" type="text" name="emailid" placeholder="Email Address" required>
         <input class="inputclass" id="mobnum" type="tel" name="ContactNum" placeholder="Contact Number" maxlength="10" oninput="reseterror3()" required>
-        <br><br>
         <input class="inputclass" id="username" type="text" name="Username" placeholder="Username" oninput="reseterror2()" required>
-        <br><br>
         <input class="inputclass" id="userpass1" type="password" name="Password" placeholder="Password" oninput="reseterror1()" required>
-        <br><br>
-        <input class="inputclass" id="userpass2" type="password" name="CPassword" placeholder="Confirm Password" oninput="reseterror1()" required><br><br>
+        <input class="inputclass" id="userpass2" type="password" name="CPassword" placeholder="Confirm Password" oninput="reseterror1()" required>
         <div style="font-family:myfont;"><input type="checkbox" onclick="toggle()">Show Password</div>
         <center>
         <p class="errormessage"><?php     echo $a=@$_GET['msg'];     ?></p>
