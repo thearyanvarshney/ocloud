1755a2dbb748a1df533fa816f7f0f84dc74a119e
Aryan Varshney <54534428+thearyanvarshney@users.noreply.github.com>
2020-06-04 09:02:59 +0530

    Update creport.php

--- creport.php
+++ creport.php
@@ -4,14 +4,17 @@ include 'dbconfig.php';
 if(isset($_REQUEST['submit'])){
     $user=$_SESSION['username'];
     $issue = $_REQUEST['issue'];
-    $que="select * from contact where binary id='$user'";
+    $status="pending";
+    $que="select * from issue where binary id='$user'";
+    $flag=0;
     $x=mysqli_query($conn,$que);
 if(mysqli_num_rows($x)==1)
 {
     header('location:report.php?msg=Again Sent Your Query');
+    $flag=1;
 }
 else{
-    $sql="INSERT INTO `issue` (`username`, `issue`) VALUES ('$user','$issue')";
+    $sql="INSERT INTO `issue` (username,issue,status) VALUES ('$user','$issue',$status)";
     mysqli_query($conn,$sql);
     header('location:report.php?msg=Query Sent Succesfully');
 }
