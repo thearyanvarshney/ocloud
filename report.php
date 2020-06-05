4008dbaea313a685ae212916b8c0ac9fac0f2269
Aryan Varshney <54534428+thearyanvarshney@users.noreply.github.com>
2020-06-05 11:10:30 +0530

    Update report.php

--- report.php
+++ report.php
@@ -1,5 +1,25 @@
 <?php require "header.php"; ?>
-<?php include('dbconfig.php');?>
+<?php
+include 'dbconfig.php';
+if(isset($_REQUEST['submit'])){
+    $user=$_SESSION['username'];
+    $issue = $_REQUEST['issue'];
+    $status="pending";
+    $que="select * from issue where binary id='$user'";
+    $flag=0;
+    $x=mysqli_query($conn,$que);
+if(mysqli_num_rows($x)==1)
+{
+    header('location:report.php?msg=Again Sent Your Query');
+    $flag=1;
+}
+else{
+    $sql="INSERT INTO `issue` (username,issue,status) VALUES ('$user','$issue',$status)";
+    mysqli_query($conn,$sql);
+    header('location:report.php?msg=Query Sent Succesfully');
+}
+}
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -12,7 +32,7 @@
     <h1>Report An Issue or Give Feedback!</h1>
     <h3>Feel free to share your problem or views with us.</h3>
 
-    <form class="formclass" action="creport.php" method="post">
+    <form class="formclass" action="report.php" method="post">
         <textarea class="input-field" type="text"  name="issue" placeholder="Describe the problem being faced, briefly! Or Give Suggestions!" rows="8" cols="80" required></textarea><br>
       <input type="submit" value="Send" name="submit" class="button">
     </form>
