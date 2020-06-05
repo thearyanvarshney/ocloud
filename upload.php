b443f6eb9216c559a9dcc8baeab819564c1f5d10
Aryan Varshney <54534428+thearyanvarshney@users.noreply.github.com>
2020-06-05 10:47:07 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,3 +1,21 @@
+<?php require "header.php"; ?>
+<?php
+include('dbconfig.php');
+if (isset($_POST['uploadfile'])) {
+    $fname = $_FILES['files']['name'];
+    $title = $_POST['title'];
+    $caption = $_POST['caption'];
+    $username = $_SESSION['username'];
+    $date = date("Y-m-d");
+    $sql = "INSERT INTO userfiles(username,title,caption,filename,date) Values('$username','$title','$caption','$fname','$date')";
+    if (mysqli_query($conn, $sql)) {
+         move_uploaded_file($_FILES['files']['tmp_name'], "Userfiles/".$username.$fname);
+        header('location:upload.php?msg=File Upload Successfully');
+    } else {
+        header('location:upload.php?msg=Error In Upload');
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
 
@@ -6,7 +24,7 @@
 </head>
 
 <body>
-  <form action="cupload.php" method="post" enctype="multipart/form-data">
+  <form action="upload.php" method="post" enctype="multipart/form-data">
 
     <h1><strong>Upload Your Files</strong></h1>
 
