57975d7012cc2f72e21b3e28eb1edc5066e5a9b0
Aryan Varshney <54534428+thearyanvarshney@users.noreply.github.com>
2020-06-05 10:50:38 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,33 @@
+<?php
+session_start();
+?>
+<?php
+ include('dbconfig.php');
+ if(isset($_REQUEST['Submit']))
+ {
+ $name=$_REQUEST['Username'];
+ $pass=md5($_REQUEST['Password']);
+ $que="select * from user where binary username='$name' and binary password='$pass'";
+ $x=mysqli_query($conn,$que);
+ $result=mysqli_fetch_assoc($x);
+
+ if($result["usertype"] == 'admin' and mysqli_num_rows($x)==1)
+ {
+   $_SESSION['username']=$name;
+   header('location:adminpanel.php');
+ }
+ else if($result["usertype"] == 'normal' and mysqli_num_rows($x)==1)
+ {
+  $_SESSION['username']=$name;
+  header('location:userinterface.php');
+ }
+ else
+ {
+ header('location:login.php?msg=Either UserName or Password is Incorrect!');
+ }
+
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -23,7 +53,7 @@
 		<div class="login-text" style="margin-top: 7%;">
 			<h1 class="heading">Login</h1>
 
-			<form action="checklogin.php" method="post" autocomplete="on">
+			<form action="login.php" method="post" autocomplete="on">
 			<input class="input-field" id="userid" type="text" placeholder="Username" name="Username" required>
 			<br><br>
 			<input class="input-field" id="userpass" type="password" placeholder="Password" name="Password" pattern=".{6,}" required>
