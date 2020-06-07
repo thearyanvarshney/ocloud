bfb4548bef4821f382f0121efa5a9a10e75b4455
Anuj Arora <anuj.arora28@gmail.com>
2020-06-07 12:34:16 +0530

    Update adminpanel.js

--- js/adminpanel.js
+++ js/adminpanel.js
@@ -1,15 +1,24 @@
 var x = document.getElementsByClassName("rightcontent");
+var y =document.getElementsByClassName("body-left-options");
 function showdivdash() {
   x[3].style.display="none";
   x[2].style.display="none";
   x[1].style.display="none";
   x[0].style.display="inline-block";
+  y[0].style="background-size: 100% 100%; padding: 10px 20px; color: white;box-shadow: 2px 3px 10px 0px rgb(0, 0, 0); background: linear-gradient(to right, #FC766A99, #5B84B166);";
+  y[3].style="none";
+  y[2].style="none";
+  y[1].style="none";
 }
 function showdivuser() {
   x[3].style.display="none";
   x[2].style.display="none";
   x[0].style.display="none";
   x[1].style.display="inline-block";
+  y[1].style="background-size: 100% 100%; padding: 10px 20px; color: white;box-shadow: 2px 3px 10px 0px rgb(0, 0, 0); background: linear-gradient(to right, #FC766A99, #5B84B166);";
+  y[3].style="none";
+  y[2].style="none";
+  y[0].style="none";
 }
 
 function showdivquery() {
@@ -17,6 +26,10 @@ function showdivquery() {
   x[1].style.display="none";
   x[0].style.display="none";
   x[2].style.display="inline-block";
+  y[2].style="background-size: 100% 100%; padding: 10px 20px; color: white;box-shadow: 2px 3px 10px 0px rgb(0, 0, 0); background: linear-gradient(to right, #FC766A99, #5B84B166);";
+  y[3].style="none";
+  y[0].style="none";
+  y[1].style="none";
 }
 
 function showdivissue() {
@@ -24,4 +37,8 @@ function showdivissue() {
   x[1].style.display="none";
   x[2].style.display="none";
   x[3].style.display="inline-block";
+  y[3].style="background-size: 100% 100%; padding: 10px 20px; color: white;box-shadow: 2px 3px 10px 0px rgb(0, 0, 0); background: linear-gradient(to right, #FC766A99, #5B84B166);";
+  y[0].style="none";
+  y[2].style="none";
+  y[1].style="none";
 }
